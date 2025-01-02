362cea9c839419764ee2423ad731410b40d3e229
AdityaAP7 <adityapalodkar@gmail.com>
2025-01-02 22:25:29 +0530

    push

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,1 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+const Carousel = ({
+    children,
+    autoPlay = false,
+    autoPlayInterval = 3000,
+    showArrows = true,
+    showIndicators = true,
+    infiniteLoop = false,
+    ...props
+  }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const timeoutRef = useRef(null);
