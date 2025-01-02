ed6c40b2ad148877f8db9ee738b0071cbc9e61c7
AdityaAP7 <adityapalodkar@gmail.com>
2025-01-02 22:34:10 +0530

    push

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@
 export { default as Carousel } from './Carousel';
 
 import React from 'react';
+import { Carousel } from 'my-react-carousel'; 
+// The import path might vary; it’s linked locally during development
