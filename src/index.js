fd10050d2a897035941ec38c6f8b3eef9419a954
AdityaAP7 <adityapalodkar@gmail.com>
2025-01-02 22:35:32 +0530

    push

--- src/index.js
+++ src/index.js
@@ -9,4 +9,10 @@ import './App.css';
 
 function App() {return (
 <div className="App">  <h1>Testing My React Carousel</h1>
-<Carousel autoPlay={true} autoPlayInterval={2000} infiniteLoop={true}> <div style={{ background: 'red', height: '200px' }}>Slide 1</div> <div style={{ background: 'green', height: '200px' }}>Slide 2</div>
+<Carousel autoPlay={true} autoPlayInterval={2000} infiniteLoop={true}> <div style={{ background: 'red', height: '200px' }}>Slide 1</div> <div style={{ background: 'green', height: '200px' }}>Slide 2</div>div style={{ background: 'blue', height: '200px' }}>Slide 3</div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default App;
