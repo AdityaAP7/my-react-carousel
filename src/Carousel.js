220f713ac2b7e5c1c8c1dcd3e3db69803b89e9a2
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-03 23:29:48 +0530

    Update Carousel.js

--- src/Carousel.js
+++ src/Carousel.js
@@ -3,6 +3,16 @@ import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import PropTypes from 'prop-types';
 
+const Carousel = ({
+    children,
+    autoPlay = false,
+    autoPlayInterval = 3000,
+    showArrows = true,
+    showIndicators = true,
+    infiniteLoop = false,
+    ...props
+  }) => {
+
 
 const Carousel = ({
     children,
