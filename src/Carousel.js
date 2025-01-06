af168366a2f1d0968c60201c75e7eadbc8d03a5a
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-06 22:49:56 +0530

    Update Carousel.js

--- src/Carousel.js
+++ src/Carousel.js
@@ -11,7 +11,14 @@ import PropTypes from 'prop-types';
      
                                      
                                              import React, { useState, useEffect, useRef } from 'react';// Jump to specific index
+import PropTypes    return () => clearTimeout(timeoutRef.current);// Handle autoPlay
+      };const goToIndex = (index) => {import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';// Jump to specific index
 import PropTypes from 'prop-types';
+            };const goToIndex = (index) => {import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';// Jump to specific index
+import PropTypes from 'prop-types';               
+                                        from 'prop-types';
                                       rt PropTypes from 'prop-types';
                                          return () => clearTimeout(timeoutRef.current);// Handle autoPlay
   useEffect(() => {if (!autoPlay) return;setCurrentIndex(index);
