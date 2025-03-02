9d5fa76704d49399ca66007b9a73130870c14bc0
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-03-02 23:41:38 +0530

    Update Carousel.js

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,3 +2,6 @@ import React, { useState, useEffect, useRef, Children } from 'react';
 import PropTypes from 'prop-types';
 /**
  * Carousel Component
+*
+ * @param {object} props - Carousel properties.
+ */
