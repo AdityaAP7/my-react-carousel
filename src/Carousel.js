700560a13755f78a7517a072186e2e63dc6c5cf8
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-06-15 23:49:52 +0530

    Update Carousel.js

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,4 +1,4 @@
-rewqtrewq98765432198765432154321qimport React, { useState, useEffect, useRef, Children } from 'react';
+1rewqtrewq98765432198765432154321qimport React, { useState, useEffect, useRef, Children } from 'react';
 w234
 e123456789
 tyui1
